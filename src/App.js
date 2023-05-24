3f5abaaacdb761089dc79af9a44f4b126d2e365b
DenisM19 <moskden6@gmail.com>
2023-05-24 22:48:32 +0300

    Mobile style

--- src/App.js
+++ src/App.js
@@ -53,17 +53,17 @@ function App() {
             appBlock.querySelector('.shadow').remove();
         });
     }
-    
+
     function openForm() {
 
     }
-    
+
     function sendEmail(e) {
         e.preventDefault();
         console.log(e.target);
-        emailjs.sendForm('service_7gedn9i', 'template_lza7lpp', e.target, 'J6QjiN7ftHvd1okXb').then((res)=> {
+        emailjs.sendForm('service_7gedn9i', 'template_lza7lpp', e.target, 'J6QjiN7ftHvd1okXb').then((res) => {
             console.log(res);
-        }).catch(err=> {
+        }).catch(err => {
             console.log(err);
         });
     }
@@ -227,7 +227,7 @@ function App() {
 
                 <div className="row justify-content-md-center mt-5">
                     <div className="col col-12 m-auto paywork">
-                        <h2 className='pay'><p>Средняя заработная плата: в России 125 000р </p>
+                        <h2 className='pay'><p>Средняя заработная плата: в России <p>125 000р,</p> </p>
                             <p>по Миру 7 000$</p>
                         </h2>
                     </div>
@@ -246,7 +246,7 @@ function App() {
                                     <p><b>Ваш педагог:</b></p>
                                     <p>Павел Иваненко.</p>
                                     <p>Со стажем в 3D - 12 лет.</p>
-                                    <p>Опыт преподавания 3D - 5 лет</p>
+                                    <p>Преподает 3D - 5 лет</p>
                                 </div>
                             </div>
                         </div>
@@ -305,7 +305,7 @@ function App() {
             <div className="block4 mt-5">
                 <div className="container destroy-bg">
                     <div className="row justify-content-md-center mt-5">
-                        <div className="col col-6 m-auto mt-5">
+                        <div className="col-sm-8 col-md-6 col-lg-6 m-auto mt-5">
                             <h2 className="module">Модули обучения :</h2>
                         </div>
                     </div>
@@ -563,7 +563,6 @@ function App() {
                                 <p>- Портфолио из 4 профессиональных работ</p>
                                 <p>- Полное индивидуальное курирование</p>
                                 <p>- Карьерное курирование по окончанию курса</p>
-                                <p>- Приятные эмоции от прохождения курса</p>
                                 <p>- Доступ к материалам после окончания курса</p>
                                 <p>- Скидки на дальнейшие курсы</p>
                             </div>
@@ -572,7 +571,7 @@ function App() {
                 </div>
 
                 <div className="col-12 mt-3 example">
-                    <h2 className="col-8 mt-5 mb-5">Работы учеников: </h2>
+                    <h2 className="col-md-8 col-lg-8 col-sm-12 mt-5 mb-5">Работы учеников: </h2>
                     <div className="uk-child-width-1-2" uk-grid="" uk-lightbox="animation: slide">
                         <div className="uk-first-column">
                             <div className="uk-child-width-1-2" uk-grid="">
@@ -689,7 +688,7 @@ function App() {
                             </div>
                         </div>
                     </div>
-                    <h2 className="col-8 mt-5 mb-5"><a href="https://www.w3schools.com">Ещё больше работ:</a></h2>
+                    <h2 className="col-md-8 col-lg-8 col-sm-12  mt-5 mb-5"><a href="https://www.w3schools.com">Ещё больше работ:</a></h2>
                 </div>
 
                 <div className="row mt-5">
@@ -719,21 +718,35 @@ function App() {
 
                                 <div className="order-contacts">
                                     <button className="open-form">Оставить заявку на звонок</button>
-                                    <div className="contacts">
+
                                         <p>Или просто пиши нам по телефону:</p>
                                         <div className="social">
                                             <a href="tel:+79779925425" target="_blank"
-                                               rel="nofollow noopener">+79779925425</a>
+                                               rel="nofollow noopener">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                                     fill="currentColor" className="bi bi-telephone-inbound"
+                                                     viewBox="0 0 16 16">
+                                                    <path
+                                                        d="M15.854.146a.5.5 0 0 1 0 .708L11.707 5H14.5a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5v-4a.5.5 0 0 1 1 0v2.793L15.146.146a.5.5 0 0 1 .708 0zm-12.2 1.182a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
+                                                </svg>
+                                                +79779925425</a>
                                             <a href="https://wa.me/79779925425" target="_blank" rel="nofollow noopener">
-                                                <img alt="WhatsApp"/>
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                                     fill="currentColor" className="bi bi-whatsapp" viewBox="0 0 16 16">
+                                                    <path
+                                                        d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z"/>
+                                                </svg>
                                                 WhatsApp
                                             </a>
                                             <a href="https://t.me/DMask124" target="_blank" rel="nofollow noopener">
-                                                <img alt="Telegram"/>
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                                     fill="currentColor" className="bi bi-telegram" viewBox="0 0 16 16">
+                                                    <path
+                                                        d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.287 5.906c-.778.324-2.334.994-4.666 2.01-.378.15-.577.298-.595.442-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294.26.006.549-.1.868-.32 2.179-1.471 3.304-2.214 3.374-2.23.05-.012.12-.026.166.016.047.041.042.12.037.141-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8.154 8.154 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629.093.06.183.125.27.187.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.426 1.426 0 0 0-.013-.315.337.337 0 0 0-.114-.217.526.526 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09z"/>
+                                                </svg>
                                                 Telegram
                                             </a>
                                         </div>
-                                    </div>
                                 </div>
 
                                 <p>Созвонись и проконсультируйся подходит ли твоя техника для занятий.</p>
@@ -751,7 +764,7 @@ function App() {
                 <form className="form-data  border  m-auto p-3" onSubmit={sendEmail}>
                     <h1>Контакты для связи</h1>
                     <div className="form-group">
-                        <h5 >Имя *:</h5>
+                        <h5>Имя *:</h5>
                         <input type='text' className='form-control' name='name' placeholder='Ваше имя'/>
                     </div>
                     <div className="form-group">
