4e913680bea5a9e143e25be3761aaaf8f5cb5dbd
MoskalenkoD <moskden6@gmail.com>
2023-05-30 15:23:18 +0300

    fonts style

--- src/App.js
+++ src/App.js
@@ -243,7 +243,7 @@ function App() {
                 <div className="row justify-content-md-center mt-5">
                     <div className="col col-12 m-auto paywork">
                         <h2 className='pay'>
-                            <p>Средняя заработная плата: в России <p>125 000р,</p></p>
+                            <p>Средняя заработная плата: в России <p>125 000р, </p></p>
                             <p>по Миру 7 000$</p>
                         </h2>
                     </div>
@@ -261,8 +261,17 @@ function App() {
                                 <div className="text">
                                     <p><b>Ваш педагог:</b></p>
                                     <p>Павел Иваненко.</p>
-                                    <p>Со стажем в 3D - 12 лет.</p>
+                                    <p>Стаж в 3D - 12 лет.</p>
                                     <p>Преподает 3D - 5 лет</p>
+                                    <a href='https://www.linkedin.com/in/pavel-ivanenko-4178b0b4' target="_blank"
+                                       rel="nofollow noopener" >
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                             fill="currentColor" className="bi bi-linkedin" viewBox="0 0 16 16">
+                                            <path
+                                                d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
+                                        </svg>
+                                        LinkedIn
+                                    </a>
                                 </div>
                             </div>
                         </div>
@@ -644,7 +653,7 @@ function App() {
                                 </div>
                                 <div>
                                     <div className="uk-width-1-1 uk-height-small p-3">
-                                        <a className="uk-inline" href="./source/slider/551.jpg" style={{width: '100%'}}
+                                        <a className="uk-inline" href="./source/slider/551.jpg" style={{overflow: 'hidden', width: '100%', height: '100%'}}
                                            data-caption="Example 6">
                                             <img src="./source/slider/551.jpg" width="1800" height="1200" alt=""
                                                  style={{width: '100%', height: '100%', objectFit: 'cover'}}/>
@@ -654,7 +663,7 @@ function App() {
                             </div>
                             <div className="uk-child-width-1-1" uk-grid="">
                                 <div className="uk-width-1-1 uk-height-small p-3">
-                                    <a className="uk-inline" href="./source/slider/660.jpg" style={{overflow: 'hidden'}}
+                                    <a className="uk-inline" href="./source/slider/660.jpg" style={{overflow: 'hidden', height: '100%'}}
                                        data-caption="Example 7">
                                         <img src="./source/slider/660.jpg" width="1800" height="1200" alt=""
                                              style={{marginTop: '-10%'}}/>
@@ -664,7 +673,7 @@ function App() {
                             <div className="uk-child-width-1-2" uk-grid="">
                                 <div className="uk-first-column">
                                     <div className="uk-width-1-1 uk-height-medium p-3">
-                                        <a className="uk-inline" href="./source/slider/221.jpg" style={{height: '100%'}}
+                                        <a className="uk-inline" href="./source/slider/221.jpg" style={{overflow: 'hidden', height: '100%'}}
                                            data-caption="Example 8">
                                             <img src="./source/slider/221.jpg" width="1800" height="1200" alt=""
                                                  style={{
